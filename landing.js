0bfc564942cc6335bb90c6c27a4ff6f67a28e551
MrFred17 <57257280+MrFred17@users.noreply.github.com>
2020-07-06 12:43:16 +0300

    timing changes on landing

--- landing.js
+++ landing.js
@@ -1,36 +1,40 @@
 const tellMeMore = document.querySelector(".a")
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.1;
-}, 7300 );
 
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.2;
-}, 7500 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.3;
-}, 7700 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.4;
-}, 7800 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.5;
-}, 7900 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.6;
-}, 8100 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.7;
-}, 8300 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.8;
-}, 8500 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 0.9;
-}, 8700 );
-setTimeout(function(){
-    tellMeMore.style.opacity = 1;
-    tellMeMore.style.pointerEvents = "all";
-}, 8900 );
+function startShowing() {
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.1;
+    }, 7600 );
+    
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.2;
+    }, 7900 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.3;
+    }, 8100 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.4;
+    }, 8300 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.5;
+    }, 8500 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.6;
+    }, 8700 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.7;
+    }, 8900 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.8;
+    }, 9100 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 0.9;
+    }, 9300 );
+    setTimeout(function(){
+        tellMeMore.style.opacity = 1;
+        tellMeMore.style.pointerEvents = "all";
+    }, 9500 );
+}
+
 
 
 const pages = document.querySelectorAll(".page");
@@ -55,6 +59,7 @@ const loaderInner = document.querySelector(".loader-inner");
 window.addEventListener("load", () => {
     // loader.classList.add("hide");
     loaderWrapper.classList.add("hide");
+    startShowing();
     // loaderInner.classList.add("hide");
 })
 
