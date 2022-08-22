efc9cd8f676d558f3a03126dcab842a7bca6b34d
MrFred17 <nadav.saragossi@gmail.com>
2022-08-22 05:12:00 +0300

    Landing page bug fixes

--- landing.js
+++ landing.js
@@ -1,39 +1,39 @@
 const tellMeMore = document.querySelector(".a")
 
-function startShowing() {
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.1;
-    }, 7400 );
+// //function startShowing() {
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.1;
+//     }, 7400 );
     
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.2;
-    }, 7600 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.3;
-    }, 8000 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.4;
-    }, 8300 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.5;
-    }, 8500 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.6;
-    }, 8700 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.7;
-    }, 8900 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.8;
-    }, 9000 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 0.9;
-    }, 9100 );
-    setTimeout(function(){
-        tellMeMore.style.opacity = 1;
-        tellMeMore.style.pointerEvents = "all";
-    }, 9100 );
-}
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.2;
+//     }, 7600 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.3;
+//     }, 8000 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.4;
+//     }, 8300 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.5;
+//     }, 8500 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.6;
+//     }, 8700 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.7;
+//     }, 8900 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.8;
+//     }, 9000 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 0.9;
+//     }, 9100 );
+//     setTimeout(function(){
+//         tellMeMore.style.opacity = 1;
+//         tellMeMore.style.pointerEvents = "all";
+//     }, 9100 );
+// }
 
 
 
@@ -59,7 +59,7 @@ const loaderInner = document.querySelector(".loader-inner");
 window.addEventListener("load", () => {
     // loader.classList.add("hide");
     loaderWrapper.classList.add("hide");
-    startShowing();
+    //startShowing();
     // loaderInner.classList.add("hide");
 })
 
